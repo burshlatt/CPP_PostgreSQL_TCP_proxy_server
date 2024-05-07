d6e29d53d300d806bfaebd9220bb800efccfde28
Bulat <61879413+burshlatt@users.noreply.github.com>
2024-05-07 09:05:34 +0300

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc == 2) {
-        Server server(std::stoi(argv[1]));
+        Server server(argv[1]);
         server.Start();
     } else {
         std::cerr << "Usage: " << argv[0] << " <port>\n";
